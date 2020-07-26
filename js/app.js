0758ff0a387adf17efc2ad522abd1852b1be180b
margare-jihua <margaret.jihua@gmail.com>
2020-07-26 16:45:10 -0700

    Get Slide show starts

--- js/app.js
+++ js/app.js
@@ -1,37 +1,35 @@
 const body = document.querySelector('body')
 const searchBar = document.querySelector('.search-bar')
 const submit = document.querySelector('.search-btn')
-let slidesCount = 0
+const slideShow = document.querySelector('.slide-show')
+const picture = document.querySelector('.picture')
+const stopSlideShow = document.querySelector('.stop')
+let input
+let pictureUrls = []
+let slidesIndex = 0
+let interval
 
-function showSlides() {    
-    let hiddenSlides = document.querySelectorAll('.each-slide')
-    for (let i = 0; i < hiddenSlides.length; i++){
-        hiddenSlides[i].style.display = 'none';
+function changePic() {
+    console.log(pictureUrls[slidesIndex]);
+    slidesIndex ++
+    picture.setAttribute('src', pictureUrls[slidesIndex])    
+    if (slidesIndex > pictureUrls.length) {
+        slidesCount = 0
     }
-    slidesCount ++
-    if (slidesCount > hiddenSlides.length) {
-        slidesCount = 1
-    }
-    hiddenSlides[slidesCount-1].style.display = 'block';
-    console.log(slideIndex);
-    console.log(slidesCount);
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 
 submit.addEventListener('click', function() {
-    searchBar.style.display = 'none'
-    const input = document.querySelector('.search-input').value
-    console.log(input);
-
-    //need to do something with input 
-    fetch('https://www.reddit.com/search.json?q=cats+nsfw:no')
+    input = document.querySelector('.search-input').value
+    let endpoint = 'https://www.reddit.com/search.json?q=' + input + '+nsfw:no'
+    // console.log(endpoint);
+    fetch(endpoint)
     .then(response => {
         return response.json()
     })
     .then(jsonData => {
         let data = jsonData.data
         let children = data.children
-        let pictureUrls = []
+        
         children.forEach(eachChild => {
             let childData = eachChild.data
             let pictureUrl = childData.thumbnail
@@ -39,83 +37,16 @@ submit.addEventListener('click', function() {
                 pictureUrls.push(pictureUrl)
             }        
         })
-
-        // Add slideshow div
-
-        const carousel = document.createElement('div')
-        carousel.classList.add('carousel')
-
-        const slides = document.createElement('div')
-        slides.classList.add('slides')
-
-        for (let i = 0; i < pictureUrls.length; i++) {
-
-            let url = pictureUrls[i]
-
-            const eachSlide = document.createElement('div')
-            eachSlide.classList.add('each-slide')
-                
-            const image = document.createElement('img')
-            image.src = url
-            image.classList.add('pictures')
-
-            eachSlide.appendChild(image)
-            slides.appendChild(eachSlide)
-
-        }
-        
-        showSlides()
-
-        carousel.appendChild(slides)
-        body.appendChild(carousel)       
+        picture.setAttribute('src', pictureUrls[0])
+        searchBar.style.display = 'none'
+        slideShow.style.display = 'block'        
+        interval = setInterval(changePic, 2000);
     })
 })
 
- 
-        // // Add Carousel from Bootstrap
-
-        // const carousel = document.createElement('div')
-        // carousel.setAttribute('id', 'carouselExampleSlidesOnly')
-        // carousel.setAttribute('class', 'carousel slide')
-        // carousel.setAttribute('data-ride', 'carousel')
-
-        // const carouselInner = document.createElement('div')
-        // carouselInner.classList.add('carousel-inner')
-        
-        // for ( let i = 0; i < pictureUrls.length; i++) {
-        //     const carouselItem = document.createElement('div')
-        //     carouselItem.classList.add('carousel-item')
-        //     if (i === 0) {
-        //         carouselItem.classList.add('active')
-        //     }
-
-        //     const image = document.createElement('img')
-        //     image.src = pictureUrls[i]
-        //     image.setAttribute('class', 'd-block w-100')
-            
-        //     carouselItem.appendChild(image)
-        //     carouselInner.appendChild(carouselItem)
-            
-        // }
-        // console.log(carouselInner);
-        // carousel.appendChild(carouselInner)
-        // body.appendChild(carousel)
-        // console.log(body);
-
-//     <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-//   <div class="carousel-inner">
-//     <div class="carousel-item active">
-//         <img src="..." class="d-block w-100" alt="...">
-//     </div>
-//     <div class="carousel-item">
-//       <img src="..." class="d-block w-100" alt="...">
-//     </div>
-//     <div class="carousel-item">
-//       <img src="..." class="d-block w-100" alt="...">
-//     </div>
-//   </div>
-// </div>
-
-//.carousel('pause')
-
-
+stopSlideShow.addEventListener('click', function() {
+    slideShow.style.display = 'none'
+    searchBar.style.display = 'block'
+    pictureUrls = []
+    clearInterval(interval)
+})
